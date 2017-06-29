c4adc06f0d93075333866468527285b0561a7388
Fa-v <fabiolacvieyra@gmail.com>
2017-06-29 17:52:01 +0200

    adding timer

--- src/App.js
+++ src/App.js
@@ -2,23 +2,107 @@ import React, { Component } from 'react';
 import './index.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    let timer;
+
+    this.state = {
+      sessionLength: 25,
+      breakLength: 5,
+      displayTime: '25:00',
+      remainingTime: 1500,
+      isRunning: false,
+      mode: 'session'
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.ticker = this.ticker.bind(this);
+  }
+
+  handleChange(event) {
+    const { sessionLength, breakLength, mode } = this.state;
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    
+    if (name === 'sessionLength') {
+      this.setState({
+        sessionLength: value,
+        displayTime: value + ':00',
+       /* remainingTime: value * 60,*/
+        mode: 'session',
+      });
+    } else {
+      this.setState({
+        breakLength: value,
+        mode: 'break',
+      });
+    }
+  }
+
+  startCountDown() {
+    const { isRunning } = this.state;
+    this.ticker();
+    this.setState({
+      isRunning: true
+    });
+  }
+
+  ticker() {
+    const { sessionLength, breakLength, remainingTime } = this.state;
+    const now = Date.now();
+    const then = now + (sessionLength * 60) * 1000;
+
+    this.timer = setInterval(() => {
+      const secondsLeft = Math.round((then - Date.now()) / 1000);
+      let displayMinutes = Math.floor(secondsLeft / 60);
+      let displaySeconds = secondsLeft % 60;
+      
+      if (secondsLeft < 0) {
+        clearInterval(this.timer);
+        this.setState({
+          isRunning: false
+        })
+      } else {
+        this.setState({
+          remainingTime: secondsLeft,
+          displayTime: displayMinutes + ':' + displaySeconds
+        });       
+      }
+    }, 1000)
+
+  }
 
   render() {
+    const { sessionLength, breakLength, displayTime, isRunning } = this.state;
+
     return (
       <div className="App">
         <h4 className="pomodoro-clock">Pomodoro Clock</h4>
         <div className="time-controls">
           <form>
-            Session length <input type="number" value="25" min="0" max="60"/>
-            Break length <input type="number" value="5" min="0" max="20"/>
+            Session length
+              <input
+                type="text"
+                name="sessionLength"
+                value={sessionLength}
+                onChange={this.handleChange} /><br/>
+            Break length
+              <input
+              type="text"
+              name="breakLength"
+              value={breakLength}
+              onChange={this.handleChange}/> 
           </form>
         </div>
 
         <div className="display-time">
           <h2 className="time-left">Time left</h2>
-          <h1>25:00</h1>
-          <button type="button">Start</button>
-          <button type="button">Reset</button>
+          <h1>{ displayTime }</h1>
+          {(isRunning === false) ? <button type="button" onClick={() => this.startCountDown()}>Start</button>
+            : <button type="button" onClick={() => this.onPause()}>Pause</button>}
+          <button type="button" onClick={() => this.resetClock()}>Reset</button>
         </div>
       </div>
     );
